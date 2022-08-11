b86d51dced8f54e2a11a44e568da26158892de7a
imkarthi16 <karthiveera@gmail.com>
2022-08-11 18:47:15 +0530

    Testing Asynchronous code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Greeting/>
+    <div className='App'>
+      <Async />
+      <Greeting />
     </div>
   );
 }
